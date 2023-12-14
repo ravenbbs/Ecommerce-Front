1696dc4ca0e95368d1b53af0c454ee1d5b7cb6da
Julio Condor <julioc3bgud@gmail.com>
2023-12-14 11:47:03 -0500

    Agregando mongoose y conenctando products

--- components/Featured.js
+++ components/Featured.js
@@ -1,51 +1,114 @@
-
-import { Carousel } from 'flowbite-react';
+import { Carousel } from "flowbite-react";
 import Image from "next/image";
 
 export default function Featured() {
   return (
-
- 
-     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel slide={false}>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
-      </Carousel>
+    <div className="lg:px-20 md:px-6 px-4 md:py-12 py-8 bg-white mt-4 mx-4 md:mx-12 rounded-md">
+      <div className="lg:flex items-center justify-between">
+        <div className="lg:w-1/3">
+          <h1 className="text-4xl font-semibold leading-9 text-gray-800 dark:text-white">
+            Indoor Interiors
+          </h1>
+          <p className="text-base leading-6 mt-4 text-gray-600 dark:text-gray-100">
+            Get inspired by our curated selection of luxiwood interiors. We hope
+            get inspired to have luxiwood interior yourself. You’ll find tips
+            here where you can buy a lot of cool furniture.
+          </p>
+          <button
+            role="button"
+            aria-label="view catalogue"
+            className=" focus:ring-gray-700  mt-6 md:mt-8 text-base font-semibold  text-gray-800 dark:text-white flex items-center "
+          >
+            Agregar al Carrito
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="lg:w-7/12 lg:mt-0 mt-8">
+          <div className="w-full h-full bg-red-200">
+            <img
+              src="https://i.ibb.co/cbyDY74/pexels-max-vakhtbovych-6782351-1-1.png"
+              alt="apartment design"
+              className="w-full sm:block hidden"
+            />
+            <img
+              src="https://i.ibb.co/ZVPGjGJ/pexels-max-vakhtbovych-6782351-1.png"
+              alt="apartment design"
+              className="sm:hidden block w-full"
+            />
+          </div>
+          <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 lg:gap-8 gap-6 lg:mt-8 md:mt-6 mt-4">
+            <img
+              src="https://i.ibb.co/4Jrp5TB/pexels-max-vakhtbovych-6782370-1.png"
+              className="w-full"
+              alt="kitchen"
+            />
+            <img
+              src="https://i.ibb.co/0Jv3FSy/pexels-max-vakhtbovych-6436799-1-1.png"
+              className="w-full"
+              alt="sitting room"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-
-)
+  );
 }
-  
 
-    // <div className="bg-white m-4 p-2 rounded-md grid grid-cols-2 gap-12 md:mx-12 md:rounded-lg  max-sm:grid-rows-2 max-sm:grid-cols-1 max-sm:">
-    //   <div className="h-fit  border-2">
-    //     <h1>Producto Destacado</h1>
-    //     <button>Agregar al Carrito</button>
-    //     <button>Leer Mas</button>
-    //   </div>
-    //   <div>
-    //     <img className="" src="https://t4.ftcdn.net/jpg/06/01/14/23/360_F_601142328_VnY6DMf1sC0RULodemaCSrvXSlFhO1lA.jpg" />
-    //   </div>
-    // </div>
+{
+  /* <div className="mt-4 mx-4 max-w-2xl bg-white rounded-xl shadow-md overflow-hidden md:max-w-full md:mx-12 ">
+  <div className="md:flex">
+    <div className="md:shrink-0 p-2 min-h-fit md:h-80 md:w-">
+      <img className=" w-full object-cover md:h-full " src="https://t4.ftcdn.net/jpg/06/01/14/23/360_F_601142328_VnY6DMf1sC0RULodemaCSrvXSlFhO1lA.jpg" alt="Modern building architecture"/>
+    </div>
+    <div className="p-8 max-sm:h-44">
+      <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+      <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
+      <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+    </div>
+  </div>
+</div> */
+}
 
-    //el codigo parecido al figma
-    // <div className="flex justify-center w-full">
-    //   <div className="p-4 border-2 md:mx-12 w-full ">
-    //     <div className="block relative h-96  overflow-hidden ">
-    //       <div className="absolute  border-4 h-full w-full pl-24 pt-12">
-    //         <h1 className="font-semibold text-4xl mb-20">Tendencia - Novedades</h1>
-    //         <button className="bg-white px-12 py-2 rounded-md font-semibold">Ver mas</button>
-    //       </div>
+//   <div className="bg-white m-4 p-2 rounded-md flex flex-wrap gap-12 md:mx-12 md:rounded-lg   max-sm:grid-cols-1 max-sm:">
+//   <div className="max-sm:grow min-h-full border-2 w-full">
+//     <h1>Producto Destacado</h1>
+//     <p>lorem ipssdaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa </p>
+//     <button>Agregar al Carrito</button>
+//     <button>Leer Mas</button>
+//   </div>
+//   <div className='w-full'>
+//     <img className="" src="https://t4.ftcdn.net/jpg/06/01/14/23/360_F_601142328_VnY6DMf1sC0RULodemaCSrvXSlFhO1lA.jpg" />
+//   </div>
+// </div>
 
-    //       <img
-    //         className="w-full "
-    //         src={
-    //           "https://img.freepik.com/fotos-premium/auriculares-inalambricos-banco-energia-bateria-portatil-externo-sobre-fondo-azul-rosa-vista-superior-plano_175682-34486.jpg?w=826"
-    //         }
-    //       />
-    //     </div>
-    //   </div>
-    // </div>
+//el codigo parecido al figma
+// <div className="flex justify-center w-full">
+//   <div className="p-4 border-2 md:mx-12 w-full ">
+//     <div className="block relative h-96  overflow-hidden ">
+//       <div className="absolute  border-4 h-full w-full pl-24 pt-12">
+//         <h1 className="font-semibold text-4xl mb-20">Tendencia - Novedades</h1>
+//         <button className="bg-white px-12 py-2 rounded-md font-semibold">Ver mas</button>
+//       </div>
+
+//       <img
+//         className="w-full "
+//         src={
+//           "https://img.freepik.com/fotos-premium/auriculares-inalambricos-banco-energia-bateria-portatil-externo-sobre-fondo-azul-rosa-vista-superior-plano_175682-34486.jpg?w=826"
+//         }
+//       />
+//     </div>
+//   </div>
+// </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-export default function Home() {
+export default function Home({product}) {
+  console.log(product)
   return (
     <div>
     <Header/>
     <Featured/>
     </div>
   )
+}
+
+
+export async function getServerSideProps(){
+
+  const featuredProductId = '6579daddf0f819e6cae583aa'
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId)
+  return {
+    props: {product: JSON.parse(JSON.stringify(product))}
+  }
 }
