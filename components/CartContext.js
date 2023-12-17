5837506b4f5b9a48cede8b92280a8f3026cb770f
Julio Condor <julioc3bgud@gmail.com>
2023-12-16 22:35:01 -0500

    Detallando el cart

--- components/CartContext.js
+++ components/CartContext.js
@@ -22,8 +22,19 @@ export function CartContextProvider({children}){
   function addProduct(productId){
     setCartProducts(prev => [...prev, productId])
   }
+
+  function removeProduct(productId){
+    setCartProducts(prev => {
+      const pos = prev.indexOf(productId)
+      if(pos !== -1){
+       return prev.filter((value, index) => index !== pos )
+      } else {
+        return prev;
+      }
+    })
+  }
   
   return(
-    <CartContext.Provider value={{cartProducts, setCartProducts, addProduct}}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{cartProducts, setCartProducts, addProduct, removeProduct}}>{children}</CartContext.Provider>
   )
 }
--- pages/cart.js
+++ pages/cart.js
@@ -1,11 +1,11 @@
 import { CartContext } from "@/components/CartContext";
 import Header from "@/components/Header";
+import Input from "@/components/Input";
 import axios from "axios";
-import Image from "next/image";
 import { useContext, useEffect, useState } from "react";
 
 export default function CartPage() {
-  const { cartProducts } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct, productId } = useContext(CartContext);
   const [products, setProducts] = useState([]);
   useEffect(() => {
     if (cartProducts.length > 0) {
@@ -15,54 +15,91 @@ export default function CartPage() {
     }
   }, [cartProducts]);
 
+  function moreOfThisProduct(id){
+    addProduct(id)
+  }
+  function lessOfThisProduct(id){
+    removeProduct(id)
+  }
+  let productsTotal = 0;
+  for (const productId of cartProducts) {
+    const price = products.find(p => p._id === productId)?.price || 0;
+    productsTotal += price;
+  }
+
   return (
     <>
-      <Header hidden={" hidden"} />
+      <Header hidden={"hidden"} />
 
-      <section className="grid grid-cols-2 pt-12 px-4 gap-5">
-        <div className="bg-white shadow rounded-lg  w-full max-w-md pt-6 ">
-          <h2>Carrito</h2>
+      <section className="mb-6 flex pt-12 px-4 gap-5 max-md:grid-cols-1 max-md:grid justify-center ">
+        <div className=" bg-white shadow rounded-lg  w-full max-w-2xl pt-6 max-md:mx-auto px-4 ">
+          <h2 className="text-2xl font-bold mb-6 mx-4">Carrito</h2>
           {!cartProducts.length && (
-            <div key={cartProducts.length} className="w-full h-20 text-center py-6 font-semibold text-xl">
+            <div
+              key={cartProducts.length}
+              className="w-full h-20 text-center py-6 font-semibold text-xl"
+            >
               El carrito esta vació
             </div>
           )}
 
           {products.length > 0 && (
-            <table>
+            <table className="w-full">
               <thead>
-                <tr>
+                <tr className="text-gray-400 text-left border-b-2">
                   <th>Productos</th>
-                  <th>Cantidad</th>
-                  <th>Precio</th>
+                  <th className="text-center">Cantidad</th>
+                  <th className="text-center">Precio</th>
                 </tr>
               </thead>
 
               <tbody>
                 {products.map((product) => (
-                  <tr  key={product._id}>
-                    
+                  <tr key={product._id} className=" border-b-2  ">
                     <td className="flex">
-                      <div className="w-46 h-46 border-2">
-                      <img className="w-46 h-46 " src={product.images[0]}  alt={`${product.title} thumbnail`}/>  
+                      <div className="mt-2">
+                        <img
+                          className=" border-2  p-2 rounded-md lg:w-40 w-32 max-h-40 max-sm:w-20"
+                          src={product.images[0]}
+                          alt={`${product.title} thumbnail`}
+                        />
+                        <h1 className="py-1 font-semibold my-2">
+                          {product.title}
+                        </h1>
                       </div>
-                      <h1>{product.title}</h1>
-                  
-                      </td>
-                    <td>
-                      {cartProducts.filter((id) => id === product._id).length}
                     </td>
-                    <td>${product.price}</td>
+                    <td className="text-center">
+                      <div className="font-bold">
+                      <button 
+                      onClick={() => moreOfThisProduct(product._id)}
+                      
+                      className="border-2 rounded-s-md w-6 border-gray-300">+</button> 
+                        <span className="mx-1">{cartProducts.filter((id) => id === product._id).length}</span>
+                      <button 
+                      onClick={() => lessOfThisProduct(product._id)}
+                      className="border-gray-300 rounded-e-md border-2 w-6">-</button>
+                        </div>             
+                    </td>
+                    <td className="text-center">
+                      $
+                      {(cartProducts.filter((id) => id === product._id).length *
+                        product.price).toFixed(2)}
+                    </td>
                   </tr>
                 ))}
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>{productsTotal}</td>
+                </tr>
               </tbody>
             </table>
           )}
         </div>
 
         {!!cartProducts.length && (
-          <div className="bg-white shadow rounded-lg h-20 w-full max-w-md">
-            dsd
+          <div className="bg-white shadow rounded-lg h-20 w-full max-w-2xl max-md:mx-auto">
+            <Input />
           </div>
         )}
       </section>
