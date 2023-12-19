2ab28f66e58b3259a0f26662e4eb0fb160b63fa0
Julio Condor <julioc3bgud@gmail.com>
2023-12-19 17:16:08 -0500

    Agregando filtros en las category page

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -5,20 +5,30 @@ import { Product } from "@/models/Product";
 
 export default function CategoryPage({ category, products }) {
   return (
-  <>
-  
-  <Header />
-    <div className=" lg:px-20 md:px-6 px-4 md:py-12 py-8 bg-white mt-4 mx-4 lg:mx-12 rounded-md  ">
-      
-
-      <h1 className="mb-2">{category.name}</h1>
-      <hr className=" mb-6 border border-blue-200 rounded-full" />
-
-      <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 mb-6">
-        {products?.length > 0 &&
-          products.map((product, index) => <ProductBox {...product} />)}
+    <>
+      <Header />
+      <div className=" lg:px-20 max-sm:px-0  px-4 md:py-12 py-8 mt-4 mx-4 lg:mx-12 rounded-md  ">
+        <div className="flex justify-between  items-baseline ">
+          <h1 className="mb-2">{category.name}</h1>
+          <div className="flex gap-8 max-sm:gap-4 items-baseline w-fit max-w-sm ">
+            {category.properties.slice(0, 2).map((prop) => (
+              <div className="bg-blue-200 px-1 py-1 mb-2 rounded-md font-semibold">
+                {prop.name}
+                <select className="bg-white rounded-sm block py-0  px-0 text-sm font-semibold text-gray-800 bg-transparent border-none  ">
+                  {prop.values.map((val) => (
+                    <option className="font-semibold" value={val}>{val}</option>
+                  ))}
+                </select>
+              </div>
+            ))}
+          </div>
+        </div>
+        <hr className=" mb-6 border border-blue-300 rounded-full" />
+        <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 mb-6">
+          {products?.length > 0 &&
+            products.map((product, index) => <ProductBox {...product} />)}
+        </div>
       </div>
-    </div>
     </>
   );
 }
@@ -28,7 +38,7 @@ export async function getServerSideProps(context) {
   const subCategories = await Category.find({ parent: category._id });
   const catIds = [category._id, ...subCategories.map((c) => c._id)];
   const products = await Product.find({ category: catIds });
-  console.log(category)
+  console.log(category);
   return {
     props: {
       category: JSON.parse(JSON.stringify(category)),
