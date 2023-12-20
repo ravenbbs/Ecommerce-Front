a3f4335324bce0ef16fc7b7b47f4ed7f080206e3
Julio Condor <julioc3bgud@gmail.com>
2023-12-20 14:27:07 -0500

    agregando mongodb y next auth

--- pages/account.js
+++ pages/account.js
@@ -5,6 +5,22 @@ export default function AccountPage(){
   return(
     <>
     <Header hidden={'hidden'} accountHidden={'hidden'}/>
+    <section className="mb-16 flex pt-12 px-4 gap-5 max-md:grid-cols-1 max-md:grid justify-center ">
+        <div className=" bg-white shadow rounded-lg  w-full max-w-2xl pt-6 max-md:mx-auto px-4 ">
+          <h1 className="mb-6 mx-4">Carrito</h1>
+     
+        </div>
+
+        
+          <div className="bg-white shadow rounded-lg h-fit w-full max-w-xl max-md:mx-auto p-4 ">
+            <h1 className=" mt-2 mx-4">
+              Información de Cuenta
+            </h1>
+            <hr className="border-gray-300 my-4" />
+           
+          </div>
+     
+      </section>
     </>
   )
 }
--- pages/search.js
+++ pages/search.js
@@ -3,41 +3,38 @@ import Input from "@/components/Input";
 import ProductBox from "@/components/ProductBox";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
-import {debounce} from "lodash";
+import { debounce } from "lodash";
 import Spinner from "@/components/Spinner";
 
 export default function SearchPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [phrase, setPhrase] = useState("");
+  const [products, setProducts] = useState([]);
+  const debouncedSearch = useCallback(debounce(searchProducts, 500), []);
 
-const [isLoading,setIsLoading] = useState(true);
-const [phrase, setPhrase] = useState('')
-const [products, setProducts] = useState([])
-const debouncedSearch = useCallback(
-  debounce(searchProducts, 500), []
-);
+  useEffect(() => {
+    if (phrase.length > 0) {
+      setIsLoading(true);
+      debouncedSearch(phrase);
+    } else {
+      setProducts([]);
+    }
+  }, [phrase]);
 
-useEffect(() => {
-  if (phrase.length > 0) {
-    setIsLoading(true);
-    debouncedSearch(phrase);
-  } else {
-    setProducts([]);
+  function searchProducts(phrase) {
+    axios
+      .get("/api/products?phrase=" + encodeURIComponent(phrase))
+      .then((response) => {
+        setProducts(response.data);
+        setIsLoading(false);
+      });
   }
-}, [phrase]);
-
-function searchProducts(phrase) {
-  axios.get('/api/products?phrase='+encodeURIComponent(phrase))
-    .then(response => {
-      setProducts(response.data);
-      setIsLoading(false);
-    });
-}
   return (
     <>
       <Header hidden={"hidden"} searchHidden={" hidden"} />
 
-      <div className="   flex justify-center  p-4 ">        
-      
-      <Input
+      <div className="   flex justify-center  p-4 ">
+        <Input
           autoFocus
           className="shadow-lg rounded-lg h-fit w-full max-w-2xl max-md:mx-auto border-none"
           type="text"
@@ -47,18 +44,19 @@ function searchProducts(phrase) {
           onChange={(ev) => setPhrase(ev.target.value)}
         />
       </div>
-        {!isLoading && phrase !== '' && products.length === 0 && (
-              <h1 className="block text-center mx-auto my-12 ">No se encontraron productos: "{phrase}"</h1>
-            )}
-        {isLoading && (
-          <Spinner fullWidth={true} />
-        )}
+      {!isLoading && phrase !== "" && products.length === 0 && (
+        <h1 className="block text-center mx-auto my-12 ">
+          No se encontraron productos: "{phrase}"
+        </h1>
+      )}
+      {isLoading && <Spinner fullWidth={true} />}
       <div className=" transition-all mx-auto px-4 max-w-4xl grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 mb-6">
-      
-        {!isLoading && products.length > 0 && 
-         products.map((product, index) => <ProductBox key={product._id} {...product} />)}
-         
-        </div>
+        {!isLoading &&
+          products.length > 0 &&
+          products.map((product, index) => (
+            <ProductBox key={product._id} {...product} />
+          ))}
+      </div>
     </>
   );
 }
